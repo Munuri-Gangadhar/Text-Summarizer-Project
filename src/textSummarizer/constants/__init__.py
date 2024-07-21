76379cc8cc5c20b3295943a90be9aad8997920a3
Munuri-Gangadhar <munurigangadhar0987@gmail.com>
2024-07-21 21:05:57 +0530

    data ingestion

---
+++ src/textSummarizer/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+# CONFIG_FILE_PATH=Path("D:/NEW/PYTHON/NLP/Text-Summarizer-Project/config/config.yaml")
+# PARAMS_FILE_PATH=Path("D:/NEW/PYTHON/NLP/Text-Summarizer-Project/params.yaml")
+
+CONFIG_FILE_PATH=Path("config/config.yaml")
+PARAMS_FILE_PATH=Path("params.yaml")
